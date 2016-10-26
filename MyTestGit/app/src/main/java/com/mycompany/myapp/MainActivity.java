8bb04f1ac6eb86d8cee859fefcf72d9d9c6f2ca8
FrancescoMarcon <francesco.marcon.2@studenti.unipd.it>
2016-10-26 19:57:42 +0200

    prende forma

--- MyTestGit/app/src/main/java/com/mycompany/myapp/MainActivity.java
+++ MyTestGit/app/src/main/java/com/mycompany/myapp/MainActivity.java
@@ -2,13 +2,60 @@ package com.mycompany.myapp;
 
 import android.app.*;
 import android.os.*;
+import android.media.*;
+import android.widget.Button;
+import android.content.Intent;
+import android.media.MediaPlayer;
 
 public class MainActivity extends Activity 
 {
+	private MediaPlayer audio=new MediaPlayer();
+	private static String traccia=null;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+		traccia=
+		
     }
+	public void plays(View v,int bpm)
+	{new BackgroundTask.execute(bpm);}
+	private class BackgroundTask extends AsyncTask<int,void,void>
+	{
+
+		@Override
+		protected Object doInBackground(Object[] p1)
+		{
+			// TODO: Implement this method
+			
+			
+			return null;
+		}
+
+		@Override
+		protected void onProgressUpdate(Object[] values)
+		{
+			// TODO: Implement this method
+			super.onProgressUpdate(values);
+		}
+
+		@Override
+		protected void onPostExecute(Object result)
+		{
+			// TODO: Implement this method
+			super.onPostExecute(result);
+		}
+
+		@Override
+		protected void onPreExecute()
+		{
+			// TODO: Implement this method
+			super.onPreExecute();
+		}
+
+		
+	}
+	
 }
